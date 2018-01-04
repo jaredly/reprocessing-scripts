b9e9cbe4c346b88909230c37d6b008f3ff06399d
Jared Forsyth <jared@jaredforsyth.com>
2018-01-04 11:29:38 -0700

    add templates

---
+++ templates/android/app/src/main/java/com/jaredforsyth/reprocessing_example/MainActivity.java
@@ -0,0 +1,7 @@
+package com.jaredforsyth.reprocessing_example;
+
+import com.jaredforsyth.reasongl.ReasonglActivity;
+
+public class MainActivity extends ReasonglActivity {
+    // Nothing to override at the moment.
+}
